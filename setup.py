bfb08a582ee151783762f65315879067e6c08e53
Áine T. Dineen <51408125+dineenai@users.noreply.github.com>
2022-02-22 15:13:13 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='fit_receptive_field',
+      version='0.0',
+      author='Aine Dineen, Alex Wade',
+      author_email='ainedineen@cusacklab.org',
+      description='A tool to find the best fit for receptive fields that are Gabor or Gaussian in Nature.',
+      url='https://github.com/dineenai/fit_receptive_field',
+      packages=find_packages(),
+      )
